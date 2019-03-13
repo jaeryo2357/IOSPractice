b7c2e7989cb81910a11e39564fbdd71c5af07cc1
jaeryo2357 <jaeryo2357@naver.com>
2019-03-13 15:30:56 +0900

    동적 테이블 뷰 추가

--- TableView/TableView/ViewController.swift
+++ TableView/TableView/ViewController.swift
@@ -9,29 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
+    
+    let dateFormatter:DateFormatter = {
+        let format:DateFormatter=DateFormatter()
+        format.timeStyle = .medium
+        format.dateStyle = .medium
+        return format
+    }()
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
         case 0:
             return korean.count
         case 1:
             return english.count
+        case 2:
+            return Dates.count
         default:
             return 0
         }
+    }
+    @IBAction func addDate(_ sender: UIButton)
+    {
+        Dates.append(Date())
+        tableview?.reloadSections(IndexSet(2...2), with: UITableView.RowAnimation.automatic)
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cel:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIndetifier, for: indexPath)
-        let text:String = indexPath.section == 0 ?korean[indexPath.row] : english[indexPath.row]
-        cel.textLabel?.text=text
-        
+        let text:String?
+        if(indexPath.section<2){
+         text = indexPath.section == 0 ?korean[indexPath.row] : english[indexPath.row]
+        }else
+        {
+            text = self.dateFormatter.string(from: self.Dates[indexPath.row])
+        }
+         cel.textLabel?.text=text
         
         return cel
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return 3
         //2개의 section이 있다는 뜻
     }
 
@@ -39,10 +59,15 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     let korean:[String] = ["가","나","다"]
     let english:[String] = ["A","B","C"]
     
+    var Dates:[Date]=[]
+    
     @IBOutlet var tableview:UITableView?
     
     func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        if(section<2){
         return section == 0 ? "한글" : "영어"
+        }
+        return nil
     }
     
     
