9eaadee6360fb2b5657470d0a6f57c6df64b7a8e
jaeryo2357 <jaeryo2357@naver.com>
2019-08-03 20:42:31 +0900

    DoIt_Web

---
+++ DoIt_Web/DoIt_Web/ViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  DoIt_Web
+//
+//  Created by YangMinUk on 03/08/2019.
+//  Copyright © 2019 YangMinUk. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var txtUrl: UITextField!
+    @IBOutlet weak var myWebView: WKWebView!
+    @IBOutlet weak var myActivityIndicator: UIActivityIndicatorView!
+    
+    func loadWebPage(_ url: String)
+    {
+        let myUrl = URL(string: url)
+        let myRequest = URLRequest(url: myUrl!)
+        myWebView.load(myRequest)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        loadWebPage("http://2sam.net")
+        
+        myWebView.addObserver(self, forKeyPath: #keyPath(WKWebView.isLoading), options: .new, context: nil)
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "loading" {
+            
+            if myWebView.isLoading {
+                myActivityIndicator.startAnimating()
+                myActivityIndicator.isHidden = false
+            }else{
+                myActivityIndicator.startAnimating()
+                myActivityIndicator.isHidden = true
+            }
+            
+        }
+    }
+    
+    func checkUrl(_ url: String) -> String {
+        var strUrl = url
+        let flag = strUrl.hasPrefix("http://")
+        if !flag {
+            strUrl = "http://" + strUrl
+        }
+        return strUrl
+    }
+    
+    @IBAction func btnGoUrl(_ sender: UIButton) {
+        let myUrl = checkUrl(txtUrl.text!)
+        txtUrl.text = ""
+        loadWebPage(myUrl)
+    }
+    
+    @IBAction func btnGoSite1(_ sender: UIButton) {
+        loadWebPage("http://fallinmac.tistory.com")
+    }
+    
+    @IBAction func btnGoSite2(_ sender: UIButton) {
+        loadWebPage("http://blog.2sam.net")
+    }
+    
+    @IBAction func btnLoadHtmlString(_ sender: UIButton) {
+        let htmlString = " <h1> HTML String </h1><p> String 변수를 이용한 웹 페이지 </p><p><a href=\"http://2sam.net\">2sam</a>으로 이동 </p>"
+        myWebView.loadHTMLString(htmlString, baseURL: nil)
+    }
+    
+    @IBAction func btnLoadHtmlFile(_ sender: UIButton) {
+        let filePath = Bundle.main.path(forResource: "htmlView", ofType: "html")
+        let myUrl = URL(fileURLWithPath: filePath!)
+        let myRequest = URLRequest(url: myUrl)
+        myWebView.load(myRequest)
+    }
+    
+    @IBAction func btnStop(_ sender: UIBarButtonItem) {
+        myWebView.stopLoading()
+    }
+    
+    @IBAction func btnReload(_ sender: UIBarButtonItem) {
+        myWebView.reload()
+    }
+    
+    @IBAction func btnGoBack(_ sender: UIBarButtonItem) {
+        myWebView.goBack()
+    }
+    
+    @IBAction func btnGoForward(_ sender: UIBarButtonItem) {
+        myWebView.goForward()
+    }
+}
+
